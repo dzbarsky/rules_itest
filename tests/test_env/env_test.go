10f3bd3a95542653b9a2b1f0aab00368ef4d8e4b
Richard Lavoie <richard.lavoie@logmein.com>
2025-04-23 16:07:54 -0400

    service_test env fails to resolve runfile

--- tests/test_env/env_test.go
+++ tests/test_env/env_test.go
@@ -3,10 +3,28 @@ package test_env
 import (
 	"os"
 	"testing"
+
+	"github.com/bazelbuild/rules_go/go/tools/bazel"
 )
 
 func TestEnv(t *testing.T) {
 	if os.Getenv("ITEST_ENV_VAR") != "ITEST_ENV_VAR_VALUE" {
 		t.Fatal("env var not passed")
+	}
+}
+
+func TestRLocation(t *testing.T) {
+	filepath, err := bazel.Runfile(os.Getenv("ENV_RUNFILE"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	content, err := os.ReadFile(filepath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(content) != "content" {
+		t.Fatal("Content mismatch")
 	}
 }
